f4ebd3fb15efa01ebd1c15894fe5c32cb701122b
ZA Viking <78074905+ZAViking@users.noreply.github.com>
2024-10-03 13:29:28 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   typescript: { strict: false },
   github: {
     repo: 'nuxt-modules/supabase',
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     // '@nuxt/Icon',
     // '@nuxtjs/fontaine',
     "@nuxtjs/color-mode"
-    "@nuxtjs/supabase",
+    "@nuxtjs/supabase"
     // "@nuxt/image",
     // "@nuxt/scripts",
     // "@vueuse/nuxt",
